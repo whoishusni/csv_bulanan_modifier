26334eed675c4c72ff91815d6a0e0ced9fd72da3
Mohammad Husni Mubaraq <mohhusnimubaraq@gmail.com>
2025-09-29 10:10:01 +0800

    [FIX + ADD ] fix import and add init

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         version_number = 'v1.0.0'
-        self.setWindowIcon(QIcon('./icon/modifier.png'))
+        self.setWindowIcon(QIcon('icon/modifier.png'))
         self.setWindowTitle(f'CSV Bulanan Modifier_{version_number}')
         self.setFixedSize(QSize(357,300))
         self.setContentsMargins(6,6,6,6)
@@ -17,8 +17,8 @@ class MainWindow(QMainWindow):
         self.nominal_changer = NominalChanger()
         
         tab_widget = QTabWidget(self)
-        tab_widget.addTab(self.nominal_changer,QIcon('./icon/change.png'),'Nominal Changer')
-        tab_widget.addTab(self.account_splitter,QIcon('./icon/split.png'),'Account Splitter')
+        tab_widget.addTab(self.nominal_changer,QIcon('icon/change.png'),'Nominal Changer')
+        tab_widget.addTab(self.account_splitter,QIcon('icon/split.png'),'Account Splitter')
         
         layout = QVBoxLayout()
         layout.addWidget(tab_widget)
